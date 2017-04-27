ce8dc2152a508ea359159a917ea469a106065503
Daniele Galloni <d.s.galloni@gmail.com>
2017-04-27 19:14:46 +0200

    feat: Add (currently empty) neural netowrks module

---
+++ modules/neural_network.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 27 14:10:49 2017
+
+@author: daniele
+"""
+
+import tensorflow as tf
+
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
